68b7bc65abe059e6a62031b0827156d707a748c9
ts-uc <s0@ts-uc.org>
2021-10-29 17:33:50 +0900

    フック化前

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,62 @@
 import React from "react";
 
-export function Login() {
-  return <h2>Login</h2>;
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    alert(
+      "A name was submitted: \n" +
+        "email:" +
+        this.state.email +
+        "password:" +
+        this.state.password
+    );
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          e-mail:
+          <input
+            name="email"
+            type="text"
+            checked={this.state.email}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            name="password"
+            type="text"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
