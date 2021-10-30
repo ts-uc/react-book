83decdc6d245d4436999c0652c641fc61363232d
ts-uc <s0@ts-uc.org>
2021-10-31 08:25:47 +0900

    changed to use hook

--- src/Login.js
+++ src/Login.js
@@ -1,62 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
+export const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-  }
-
-  handleInputChange(event) {
+  const handleInputChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    this.setState({
+    setForm({
+      ...form,
       [name]: value,
     });
-  }
+  };
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     alert(
       "A name was submitted: \n" +
         "email:" +
-        this.state.email +
+        form.email +
         "password:" +
-        this.state.password
+        form.password
     );
     event.preventDefault();
-  }
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          e-mail:
-          <input
-            name="email"
-            type="text"
-            checked={this.state.email}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            name="password"
-            type="text"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        e-mail:
+        <input
+          name="email"
+          type="text"
+          checked={form.email}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          name="password"
+          type="text"
+          value={form.password}
+          onChange={handleInputChange}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
