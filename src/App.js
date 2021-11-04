1b91db6aa919c6962a0a59fcc3467375cde96d28
ts-uc <s0@ts-uc.org>
2021-11-04 19:54:09 +0900

    setTokenがなかったのを修正

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
           )}
         </Route>
         <Route path="/" exact>
-          {authed ? <Home token={token} /> : <Redirect to="/login" />}
+          {authed ? <Home token={token} setToken={setToken}/> : <Redirect to="/login" />}
         </Route>
       </Switch>
     </BrowserRouter>
