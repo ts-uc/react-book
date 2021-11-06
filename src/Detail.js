0309ab4cdd34904dcc7983fc524bbcf4266100d6
ts-uc <s0@ts-uc.org>
2021-11-06 20:54:28 +0900

    編集ボタンを表示するように変更

--- src/Detail.js
+++ src/Detail.js
@@ -1,24 +1,27 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 export const Detail = (props) => {
   const { id } = useParams();
   const [data, setData] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
-  axios
-    .get("https://api-for-missions-and-railways.herokuapp.com/books/" + id, {
-      headers: { Authorization: "Bearer " + props.token },
-    })
-    .then(function (response) {
-      console.log(response);
-      setData(response.data);
-      setIsLoaded(true);
-    })
-    .catch(function (error) {
-      console.log(error);
-      alert("読み込みエラー")
-    });
+  useEffect(() => {
+    axios
+      .get("https://api-for-missions-and-railways.herokuapp.com/books/" + id, {
+        headers: { Authorization: "Bearer " + props.token },
+      })
+      .then(function (response) {
+        console.log(response);
+        setData(response.data);
+        setIsLoaded(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("読み込みエラー");
+      });
+  }, []);
   if (isLoaded) {
     return (
       <>
@@ -29,10 +32,20 @@ export const Detail = (props) => {
         <p>詳細: {data.detail}</p>
         <p>レビュー: {data.review}</p>
         <p>投稿者: {data.reviewer}</p>
+        <EditButton id={id} isMine={data.isMine} />
       </>
     );
   }
-  return(
-      <><h2>Loading...</h2></>
-  )
+  return (
+    <>
+      <h2>Loading...</h2>
+    </>
+  );
+};
+
+const EditButton = (props) => {
+  if (props.isMine) {
+    return <Link to={`/edit/${props.id}`}>編集</Link>;
+  }
+  return "";
 };
